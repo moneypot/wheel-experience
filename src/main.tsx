818f99b54bc15d4e073c94e2925e2fcff85f42a1
Dan Zhou <danzho@proton.me>
2024-11-26 17:41:32 -0600

    Set iframe height

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { Store, StoreProvider } from "./Store.tsx";
 
+window.parent.postMessage({ type: "setHeight", px: 1000 }, "*");
+
 const store = new Store();
 
 createRoot(document.getElementById("root")!).render(
